f65e80a1aed0ffeb455c69299b4f71a86fa615a9
Alan Files <alangfiles@users.noreply.github.com>
2025-03-22 13:21:29 -0400

    updates to config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: "/psalmist/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173, // Default port for Vite
     open: true, // Automatically open the browser when the server starts
+  },
+  build: {
+    outDir: "docs", 
   },
 });
